77264f641c61c815eee0181fa5412c616092bc64
Victorrani <victor.ranieri90@gmail.com>
2024-11-11 19:13:40 -0300

    atualização

--- mypage.py
+++ mypage.py
@@ -9,10 +9,10 @@ st.write("Como primeiro teste será adicionado gifs.")
 
 # Exibindo os GIFs
 st.subheader("GIF ch13")
-st.image("/gifs/ch13/akara_ch13.gif", use_container_width=True)
+st.image("https://github.com/Victorrani/webpage/tree/65af2de3d32c800647a5663fbe7dd8f0b2c90692/gifs/ch13/akara_ch13.gif", use_container_width=True)
 
-st.subheader("GIF True Color")
-st.image("/gifs/true_color/akara_true_color.gif", use_container_width=True)
+#st.subheader("GIF True Color")
+#st.image("/home/victor/USP/webpage/gifs/true_color/akara_true_color.gif", use_container_width=True)
 
 # Rodapé ou mensagem de despedida
 st.write("Obrigado por visitar!")
