65af2de3d32c800647a5663fbe7dd8f0b2c90692
Victorrani <victor.ranieri90@gmail.com>
2024-11-11 19:01:45 -0300

    atualização

--- mypage.py
+++ mypage.py
@@ -4,15 +4,15 @@ import streamlit as st
 st.set_page_config(page_title="Meus GIFs", page_icon=":camera_flash:")
 
 # Título do site
-st.title("Bem-vindo ao Meu Site de GIFs!")
-st.write("Aqui você pode ver alguns dos meus GIFs favoritos.")
+st.title("App dedicado ao ciclone Akará")
+st.write("Como primeiro teste será adicionado gifs.")
 
 # Exibindo os GIFs
-st.subheader("GIF 1")
-st.image("/home/victor/Desktop/akara/gifs/akara_ch13.gif", use_container_width=True)
+st.subheader("GIF ch13")
+st.image("/gifs/ch13/akara_ch13.gif", use_container_width=True)
 
-st.subheader("GIF 2")
-st.image("/home/victor/Desktop/akara/gifs/akara_true_color.gif", use_container_width=True)
+st.subheader("GIF True Color")
+st.image("/gifs/true_color/akara_true_color.gif", use_container_width=True)
 
 # Rodapé ou mensagem de despedida
 st.write("Obrigado por visitar!")
